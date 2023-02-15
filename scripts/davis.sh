583da494ecbd926bb8373c88171795f88b34e2e6
ZhangSheng1101 <1104242629@qq.com>
2023-02-15 10:55:59 +0800

    1

--- scripts/davis.sh
+++ scripts/davis.sh
@@ -13,7 +13,7 @@ python demo_optimization.py \
             --consist_weight 0.1 \
             --initial warp \
             --flow_ckpt sintel \
-            --pred_stesp 5 \
+            --pred_steps 5 \
             --first_frame 2 \
             --second_frame 3 \
             --use_warp_im2 \
